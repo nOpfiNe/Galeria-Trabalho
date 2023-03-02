11bb724521de97a1cc9c4d0feb0fa768d50cb909
Bruna Moura <104026303+nOpfiNe@users.noreply.github.com>
2023-03-01 23:42:17 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ function trocar() {
     imgAnterior = aux
 }
 
-
 function imageGallery() {
     const highlight = document.querySelector(".gallery-hightlight");
     const previews = document.querySelectorAll(".room-preview img");
@@ -27,3 +26,28 @@ function imageGallery() {
   
 imageGallery();
 
+/*seta*/
+
+var img0 = "img_music/mitski.jpeg";
+var imgseguinte1 = "img_music/khelani.jpeg";
+var imgseguinte2 = "img_music/liniker.jpeg";
+var imgseguinte3 = "img_music/lins.jpg";
+
+function trocar(){
+    document.getElementById("imagem").src = img0;
+    let aux = img0;
+    img0 = imgseguinte1;
+    imgseguinte1 = imgseguinte2;
+    imgseguinte2 = imgseguinte3;
+    imgseguinte3 = aux;
+}
+
+function passar(){
+    document.getElementById("imagem").src = img0;
+    let aux = imgseguinte3;
+    imgseguinte3 = imgseguinte2;
+    imgseguinte2 = imgseguinte1;
+    imgseguinte1 = img0;
+    img0 = aux;
+   
+}
